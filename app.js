d3f5ac73ce5abd1d47ca2a1c9cb993a361d73c59
ayubb8255-wq <ayubb8255@gmail.com>
2026-06-19 21:14:35 +0500

    Implemented JavaScript Assignment 2 covering variables, strings, alerts, and browser output.
    Added well-structured solutions for JavaScript Assignment 2, demonstrating the use of variables, strings, alerts, and browser output through HTML and JavaScript.

---
+++ app.js
@@ -0,0 +1,71 @@
+// ======================================
+// Q.1 Declare a variable called username
+// ======================================
+
+let username;
+
+// ============================================================
+// Q.2 Declare a variable called myName & assign your Full Name
+// ============================================================
+
+let myName = "Iqra Ayub";
+
+// ======================================
+// Q.3 Display "Hello World" in alert box
+// ======================================
+
+let message = "Hello World";
+
+alert(message);
+
+// ===================================================
+// Q.4 Save student's bio data and show in alert boxes
+// ===================================================
+
+let studentName = "Iqra Ayub";
+let age = "13 years old";
+let course = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(age);
+alert(course);
+
+// =====================================================
+// Q.5 Display the following alert using one JS variable
+// =====================================================
+
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+alert(pizza);
+
+// =====================================================
+// Q.6 Declare email variable and show message in alert
+// =====================================================
+
+let email = "example@example.com";
+
+alert("My email address is " + email);
+
+// ==================================
+// Q.7 Declare a variable called book
+// ==================================
+
+let book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from the Book " + book);
+
+// =================================
+// Q.8 Display in browser through JS
+// =================================
+
+document.writeln("Yah! I can write HTML content through JavaScript");
+
+// ==============================================
+// Q.9 Store string and show in alert and browser
+// ==============================================
+
+let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(design);
+
+document.writeln(design);
